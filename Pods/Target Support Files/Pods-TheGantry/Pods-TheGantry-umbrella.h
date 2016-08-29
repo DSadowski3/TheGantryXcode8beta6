f7a940660a71abd3a15aec3f376467ff67e43d6f
DSadowski3 <dsadowski2001@Dominiks-MacBook-Pro.local>
2016-08-28 19:49:49 -0500

    Creating a social media app using Xcode8beta6 and swift3

---
+++ Pods/Target Support Files/Pods-TheGantry/Pods-TheGantry-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TheGantryVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TheGantryVersionString[];
+
---
+++ TheGantry/SignInVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheGantry
+//
+//  Created by Dominik Sadowski on 8/28/16.
+//  Copyright © 2016 Dominik Sadowski. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
