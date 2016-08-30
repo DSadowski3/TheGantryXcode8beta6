180b53e85f8aad1ad7daa6fceeeead82d9618215
DSadowski3 <dsadowski2001@Dominiks-MacBook-Pro.local>
2016-08-29 21:32:21 -0500

    authorized the app with firebase and facebook

--- Podfile
+++ Podfile
@@ -10,5 +10,6 @@ target 'TheGantry' do
     pod ‘Firebase/Auth’
     pod ‘Firebase/Database’
     pod ‘Firebase/Storage’
+    pod 'FBSDKLoginKit'
 
 end
--- TheGantry/SignInVC.swift
+++ TheGantry/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -15,6 +18,32 @@ class SignInVC: UIViewController {
     
     }
 
+    @IBAction func fbButtonPressed(_ sender: AnyObject) {
+       
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("DOMINIK: Unable to authenticat with FB - \(error)")
+            } else if result?.isCancelled == true {
+                print("DOMINIK: user cncelled fb authenticaiton")
+            } else {
+                print("DOMINIK: successfully authenticated with FB")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("DOMINIK: Unable to authenticate with Firebase")
+            } else {
+                print("DOMINIK: successfully atuhenticated with firebase")
+            }
+        })
+    }
 
 }
 
