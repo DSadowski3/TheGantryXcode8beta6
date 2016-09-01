58cfa2ab2dbed917a60906b29668380775086e61
DSadowski3 <dsadowski2001@Dominiks-MacBook-Pro.local>
2016-08-31 21:23:53 -0500

    linked IBOutlets and assigned custom classes to images

--- TheGantry/AppDelegate.swift
+++ TheGantry/AppDelegate.swift
@@ -48,10 +48,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func application(_ app: UIApplication, open url: URL, sourceApplicaiton: String?, annotation: AnyObject) -> Bool {
+    /*func application(_ app: UIApplication, open url: URL, sourceApplicaiton: String?, annotation: AnyObject) -> Bool {
         
         return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplicaiton: sourceApplicaiton, annotation: annotation)
-    }
+    }*/
 
 }
 
---
+++ TheGantry/Assets.xcassets/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  TheGantry
+//
+//  Created by Dominik Sadowski on 8/31/16.
+//  Copyright © 2016 Dominik Sadowski. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var likesLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+  
+    
+
+}
